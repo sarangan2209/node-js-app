67a6928b06fe1a6781e8b4703cd12c75f12ccdb6
sarangan2209 <sarangan@tecofize.com>
2025-06-25 11:11:50 +0530

    generate js docs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Greets a user by name.
- * @param {string} name - The name of the user
- * @returns {string} A greeting message
+ * @param {string} name - The name of the user.
+ * @returns {string} A greeting message.
  */
 function greet(name) {
     return `Hello, ${name}!`;
